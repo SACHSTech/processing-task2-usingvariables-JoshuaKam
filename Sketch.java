45d2d881acdd27e06882fa84a21a537ed084e54b
JoshuaKam <joshua.kam24@ycdsbk12.ca>
2022-02-23 11:39:28 -0500

    finished school

--- Sketch.java
+++ Sketch.java
@@ -16,7 +16,7 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(135,206,235);
   }
 
   /**
@@ -24,12 +24,36 @@ public class Sketch extends PApplet {
    */
   public void draw() {
 	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+		
+    
+    //Ground
+    fill(0,244,0);
+   ellipse(width/2,height/40*32,width/40*55,height/40*28);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    //Building
+    fill(255,255,0);
+    rect(width/40*5,height/40*15,width/40*30,height/40*15); 
+
+    //flag pole
+    fill(128,128,128);
+    rect(width/40*20,height/40*5,width/width*5,height/40* 10);  
+    
+    //flag
+    fill(244,0,0);
+    triangle(201,51,200,95,130,80);
+
+    //door
+    fill(165,42,42);
+    rect(175,230,32,67);
+
+    fill(	255, 165, 0);
+    ellipse(20, 20, 75, 75);
+
+    //Windows
+    fill(255,255,255);
+    rect(90,200,40,40);
+    rect(250,200,40,40);
+
   }
   
   // define other methods down here.
